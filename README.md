55788d7f2aa8ac6f4852718d8324e7ca1fb73c2f
LukasSonntag <lukaszombiekiller@gmail.com>
2022-07-03 04:30:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gmod-Server-Crasher
+ deadass an addon that crashes a gmod server when ran
+ I don't understand why you would use this
+
+ I made this because I was bored
+
+ literally unteasted code
---
+++ lua/autorun/autorun.lua
@@ -0,0 +1,22 @@
+function infaniteLoop()
+    local x = 0
+    while true do
+        x = x + 1
+    end
+end
+
+function OnStart()
+    local printStr = "\n\n\n\n\n\n\n\n\n\n"
+    printStr .. "SERVER HAS Gmod-Server-Crasher INSTALLED "
+    printStr .. "IF YOU ARE WONDERING WHY YOUR SERVER IS "
+    printStr .. "NOT RUNNING THEN NOW YOU HAVE IT"
+    printStr .. "\n\n\n\n\n\n\n\n\n\n"
+
+    Msg(printStr)
+
+    while true do
+        coroutine.create(infaniteLoop)
+    end
+end
+
+hook.add("OnGamemodeLoaded", "ServerCrasher_OnGamemodeLoaded", OnStart)
